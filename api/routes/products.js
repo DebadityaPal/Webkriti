da84643b39f4faad4457608689ce0060112f9ae8
DebadityaPal <debaditya.pal6@gmail.com>
2020-02-27 20:08:50 +0530

    Node Server Initialized with basic request management

---
+++ api/routes/products.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET requests to /products'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Handling POST requests to /products'
+    });
+});
+
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated Product!'
+    });
+});
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted Product!'
+    });
+});
+
+module.exports = router;
