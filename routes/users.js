6392f47c7f391bf3f43836fdb8c7a2ee9dfaa02a
DebadityaPal <debaditya.pal6@gmail.com>
2020-03-19 21:41:57 +0530

    Login forms do not lose data on submit now

--- routes/users.js
+++ routes/users.js
@@ -2,53 +2,80 @@ const express = require("express")
 const router = express.Router()
 const bcrypt = require("bcrypt")
 const mySqlConnection = require("../db/db")
+var path = require("path")
 let user
+let conditionval = "none"
+let emailval = null
+let nameval = null
+let phoneval = null
+let message = ""
 
 router.get("/", (req, res) => {
     if(!req.session.user) {
-        res.status(200).sendFile(__dirname + '/login.html')
-    } else res.status.send("Already LOgged In")
+        res.status(200).render(path.join(__dirname, './login.ejs'), {email: emailval, condition: conditionval})
+    } else res.redirect("/")
 });
 
 router.get("/register", (req, res) => {
     if (!req.session.user) {
-        res.status(200).sendFile(__dirname + '/register.html')
-    } else res.status(401).send("Not possible as you are logged in already")
+        res.status(200).render(path.join(__dirname, './register.ejs'), {email: emailval, name: nameval, phone: phoneval, condition: conditionval, message: message})
+    } else res.redirect("/")
 })
 
 router.post("/register", (req, res) => {
     const { name, email, password, password2, phone } = req.body
+    emailval = email
+    nameval = name
+    phoneval = phone
     let errors = []
   
-    if (!name || !email || !password || !password2 || !phone) {
-        errors.push({ msg: "Please enter all fields" })
-    }
-  
     if (password != password2) {
-        errors.push({ msg: "Passwords do not match" })
+        message = "Passwords do not match"
+        conditionval = "block"
+        errors.push("Passwords do not match")
     }
   
     if (password.length < 6) {
-        errors.push({ msg: "Password must be at least 6 characters" })
+        message = "Passwords needs to be longer than 6 letters"
+        conditionval = "block"
+        errors.push("Passwords needs to be longer than 6 letters")
     }
     mySqlConnection.query(
         "SELECT * FROM users WHERE email = ?",
         [email],
         (err, rows) => {
             if (err) res.status(500).send(err)
-            if (rows.length) errors.push({ msg: "Email already exists" })
+            if (rows.length) {
+                message = "Email already exists"
+                conditionval = "block"
+                errors.push("Email already exists")
+            }
             if (errors.length > 0) {
             res.statusCode = 400
-            res.send(errors)
+            res.redirect("/users/register")
             } else {
             pwdHash = bcrypt.hashSync(password, 10)
             var sql = `INSERT INTO users (name, email, phone, pwdHash) VALUES ?`
             const values = [[name, email, phone, pwdHash]]
-    
+
             mySqlConnection.query(sql, [values], function(err) {
                 if (err) res.status(500).send(err)
             })
-            res.status(200).send("successfully registered")
+            mySqlConnection.query(
+                "SELECT * FROM users WHERE email = ?",
+                [email],
+                (err, rows) => {
+                    if (err) res.status(500).send(err)
+                    user = rows[0]
+                    req.session.user = user
+                    nameval = null
+                    conditionval = "none"
+                    phoneval = null
+                    emailval = null
+                    message = ""
+                    res.redirect("/")
+                }
+            )
         }
       },
     )
@@ -56,22 +83,28 @@ router.post("/register", (req, res) => {
 
 router.post("/login", (req, res) => {
     const { email, password } = req.body
+    emailval = email
     mySqlConnection.query(
         "SELECT * FROM users WHERE email = ?",
         [email],
         (err, rows) => {
             if (err) res.status(500).send(err)
             user = rows[0]
+            console.log(user)
             if (user) {
             const result = bcrypt.compareSync(password, user.pwdHash)
             if (result) {
-                req.session.user = user
+                req.session.user = user  
+                emailval = null
+                conditionval = "none"
                 res.redirect("/")
             } else {
-                res.status(400).send("pwd incorrect")
+                conditionval = "block"
+                res.redirect("/")
             }
             } else {
-            res.status(400).send("email does not exist")
+                conditionval = "block"
+                res.redirect("/")
             }
         },
     )
@@ -80,10 +113,10 @@ router.post("/login", (req, res) => {
 router.get('/logout', (req, res) => {
     if(req.session.user) {
         req.session.destroy(() => {
-            res.status(200).send("logout successful")
+            res.status(200).redirect("/")
         })
     } else {
-        res.status(400).send("You are not logged in")
+        res.status(200).redirect("/")
     }
 })
 
