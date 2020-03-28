33b8393c5b54ea55914909e957154edc9205105f
DebadityaPal <debaditya.pal6@gmail.com>
2020-03-28 12:20:10 +0530

    CLEARDB mysql server connected

--- db/db.js
+++ db/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const mySqlConnection = mysql.createConnection({
-    host: "localhost",
-    user: "webkriti",
-    password: "webkriti",
-    database: "webkriti",
+    host: "us-cdbr-iron-east-01.cleardb.net",
+    user: "bd86227f11c666",
+    password: "7a22f0e8",
+    database: "heroku_39afc7b1d1ea309",
 });
 
 mySqlConnection.connect((err) => {
